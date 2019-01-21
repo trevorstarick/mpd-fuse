a34b3beedde841bb6f9b58f06251364c54da8858
Trevor Starick <trevor.starick@gmail.com>
2019-01-21 05:39:26 -0500

    initial Go file

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
+	_ "bazil.org/fuse/fs/fstestutil"
+	"golang.org/x/net/context"
+)
+
+// todo: provide basic fs
+// todo: provide access to remote machine
+// todo: implement basic caching
+// todo: implement limited caching (512MB)
+// todo: implement least-accessed pruning
+
+func main() {
+	mountpoint := os.Args[1]
+
+	c, err := fuse.Mount(
+		mountpoint,
+		fuse.FSName("mpd"),
+		fuse.Subtype("mpdfs"),
+		fuse.LocalVolume(),
+		fuse.VolumeName("MPD"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	err = fs.Serve(c, FS{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// check if the mount process has an error to report
+	<-c.Ready
+	if err := c.MountError; err != nil {
+		log.Fatal(err)
+	}
+}
+
+// FS implements the hello world file system.
+type FS struct{}
+
+func (FS) Root() (fs.Node, error) {
+	return Dir{}, nil
+}
+
+// Dir implements both Node and Handle for the root directory.
+type Dir struct{}
+
+func (Dir) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Inode = 1
+	a.Mode = os.ModeDir | 0555
+	return nil
+}
+
+func (Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
+	if name != "" {
+		return File{}, nil
+	}
+	return nil, fuse.ENOENT
+}
+
+var dirDirs = []fuse.Dirent{
+	{Inode: 2, Name: "file-a.flac", Type: fuse.DT_File},
+	{Inode: 2, Name: "file-b.flac", Type: fuse.DT_File},
+}
+
+func (Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+	return dirDirs, nil
+}
+
+// File implements both Node and Handle for the files.
+type File struct{}
+
+const greeting = "\n"
+
+func (File) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Inode = 2
+	a.Mode = 0444
+	a.Size = uint64(len(greeting))
+	return nil
+}
+
+func (File) ReadAll(ctx context.Context) ([]byte, error) {
+	return []byte(greeting), nil
+}
